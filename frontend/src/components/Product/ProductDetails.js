af64ca015beeff488e5d9f231e170646034cab83
rupali-12 <rupalisharma1009@gmail.com>
2024-09-21 22:23:02 +0530

    Final commit

--- frontend/src/components/Product/ProductDetails.js
+++ frontend/src/components/Product/ProductDetails.js
@@ -1,5 +1,4 @@
 import React, { Fragment, useEffect, useState } from "react";
-import Carousel from "react-material-ui-carousel";
 import "./ProductDetails.css";
 import { useSelector, useDispatch } from "react-redux";
 import {
