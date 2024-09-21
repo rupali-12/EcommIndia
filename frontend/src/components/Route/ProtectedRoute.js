c42fb9d7d8ea41df035b2019821f466fa979626a
rupali-12 <rupalisharma1009@gmail.com>
2024-09-21 22:32:11 +0530

    Final commit

--- frontend/src/components/Route/ProtectedRoute.js
+++ frontend/src/components/Route/ProtectedRoute.js
@@ -1,48 +1,8 @@
-// import { Navigate, Routes } from "react-router-dom";
-// import React, { Fragment } from "react";
-// import { useSelector } from "react-redux";
-// import { redirect, Route } from "react-router-dom";
 
-// // const ProtectedRoute = ({ Component, ...rest }) => {
-// // const ProtectedRoute = ({ Component, ...rest }) => {
-// const ProtectedRoute = ({ isAuthenticated, Component, ...rest }) => {
-//   const { loading, user } = useSelector((state) => state.user);
-
-//   return (
-//     <Fragment>
-//       {loading === false && (
-//        <Routes>
-//          <Route
-//           {...rest}
-//           render={(props) => {
-//             if (isAuthenticated === false) {
-//               // return redirect("/login");
-//               return <Navigate to={"/login"} />;
-//             }
-
-//             // if (isAdmin === true && user.role !== "admin") {
-//             //   return <Redirect to="/login" />;
-//             // }
-
-//             return <Component {...props} />
-//           }}
-//         />
-//        </Routes>
-//       )}
-//     </Fragment>
-//   );
-// };
-
-// export default ProtectedRoute;
-
-// ***************************
 import { Navigate } from "react-router-dom";
 import React from "react";
 import { useSelector } from "react-redux";
-// import { redirect, Route } from "react-router-dom";
 
-// const ProtectedRoute = ({ Component, ...rest }) => {
-// const ProtectedRoute = ({ Component, ...rest }) => {
 const ProtectedRoute = ({ isAuthenticated, isAdmin, children }) => {
   const { loading, user } = useSelector((state) => state.user);
   if (!isAuthenticated) {
